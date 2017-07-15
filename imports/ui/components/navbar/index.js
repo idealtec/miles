90a2a871d24dfc816af8956f3eb024516c55ff0e
ITS YESH <yvelkuri@idealtechnosoft.com>
2017-07-15 08:45:04 -0700

    menu closing fix

--- imports/ui/components/navbar/index.js
+++ imports/ui/components/navbar/index.js
@@ -5,13 +5,16 @@ Template.navbar.events({
     'click .navbar-burger': function (event, template) {
         template.$('.navbar-menu').toggleClass("is-active")
     },
-       'click .charts': function (event, template) {
-           console.log('Click');
-        $( "#myChart" ).toggleClass( 'is-hidden' );
+    'click .charts': function (event, template) {
+        template.$('.navbar-menu').toggleClass("is-active")
+
+        $("#myChart").toggleClass('is-hidden');
 
     },
     'click [data-social-login]'(event, template) {
         event.preventDefault();
+        template.$('.navbar-menu').toggleClass("is-active")
+
         const service = event.target.getAttribute('data-social-login'),
             options = {
                 requestPermissions: ['email']
